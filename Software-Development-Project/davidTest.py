41542edee3f9fd06bcdcdb9b9df81d5bf214837b
DavidEstebanBR <esteban1.14@hotmail.com>
2026-04-25 20:22:21 +1000

    Fix: add actual project files

---
+++ Software-Development-Project/davidTest.py
@@ -0,0 +1,70 @@
+import random
+subject=[[0,0,None],[0,0,None],[0,0,None],[0,0,None]]
+n_subject=0
+
+#Enrolment
+def enrolment():
+    global n_subject
+    global subject
+    if n_subject<4:        
+        IDs=f"{random.randint(1,999):03}"
+        print(f"\033[33mEnrolling in Subject-{IDs}\033[0m")
+        subject[n_subject][0]=IDs
+        subject[n_subject][1]=random.randint(25,100)
+        if subject[n_subject][1]<50:
+            subject[n_subject][2]="F"
+        elif subject[n_subject][1]<65:
+            subject[n_subject][2]="P"
+        elif subject[n_subject][1]<75:
+            subject[n_subject][2]="C"
+        elif subject[n_subject][1]<85:
+            subject[n_subject][2]="D"
+        else:
+            subject[n_subject][2]="HD"      
+
+        n_subject+=1
+        print(f"\033[33mYou are now enrolled in {n_subject} out of 4 Subjects\033[0m")
+    else:
+        print(f"\033[31mStudents are allowed to enrol in 4 subjects only\033[0m")
+#Enrolment
+
+
+#Withdraw
+def withdraw(ws):
+    global n_subject
+    global subject
+    if n_subject==0:
+        print(f"\033[31mThere are no subjects to withdraw\033[0m")
+    wsp=0
+    for i in range(3):
+        if subject[i][0]==ws:
+            wsp=i
+            print(f"\033[33mDroping subject {ws}\033[0m")
+            if wsp<=n_subject-1: #rewriting matrix and number of enrolled subjects
+                subject[wsp][0]=subject[n_subject-1][0]; subject[n_subject-1][0]=0
+                subject[wsp][1]=subject[n_subject-1][1]; subject[n_subject-1][1]=0
+                subject[wsp][2]=subject[n_subject-1][2]; subject[n_subject-1][2]=None
+                n_subject-=1
+            break
+        else:
+            print(f"\033[31mSubject does not exist in student enrolment\033[0m")
+            break
+
+#Withdraw
+ 
+
+#Show Enrolment
+def show_enrolment():
+    global n_subject
+    global subject
+    if n_subject ==1:
+        print(f"\033[33mShowing {n_subject} Subject\033[0m")
+        print(f"Subject: {subject[0][0]} -- Mark= {subject[0][1]} -- Grade= {subject[0][2]}")
+    elif n_subject >1:
+        print(f"\033[33mShowing {n_subject} Subjects\033[0m")
+        for i in range(n_subject):
+            print(f"Subject: {subject[i][0]} -- Mark= {subject[i][1]} -- Grade= {subject[i][2]}")
+    else:
+        print(f"\033[31mStudent does NOT have any subject enrolled\033[0m")
+
+#Show Enrolment
